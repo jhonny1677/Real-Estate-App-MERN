6c81bf652112e4ef898ed178604af436a46a1fdd
Ankit <ankit.acoolguy@gmail.com>
2025-10-30 15:30:00 +0000

    add list component with grid and list view toggle

--- client/src/components/list/List.jsx
+++ client/src/components/list/List.jsx
@@ -1,58 +1,13 @@
 import './list.scss'
 import Card from"../card/Card"
-import { useState } from 'react'
 
-function List({posts}){
-  const [currentPage, setCurrentPage] = useState(1)
-  const postsPerPage = 6
-
-  const indexOfLastPost = currentPage * postsPerPage
-  const indexOfFirstPost = indexOfLastPost - postsPerPage
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
-  const totalPages = Math.ceil(posts.length / postsPerPage)
-
-  const paginate = (pageNumber) => setCurrentPage(pageNumber)
-
+function List({ posts, viewMode = "list" }) {
   return (
     <div className='list'>
-      <div className='posts'>
-        {currentPosts.map(item=>(
+      <div className={`posts${viewMode === "grid" ? " grid-view" : ""}`}>
+        {posts.map(item=>(
           <Card key={item.id} item={item}/>
         ))}
-      </div>
-      
-      {totalPages > 1 && (
-        <div className='pagination'>
-          <button 
-            onClick={() => paginate(currentPage - 1)}
-            disabled={currentPage === 1}
-            className='pagination-btn'
-          >
-            Previous
-          </button>
-          
-          {[...Array(totalPages)].map((_, index) => (
-            <button
-              key={index + 1}
-              onClick={() => paginate(index + 1)}
-              className={`pagination-btn ${currentPage === index + 1 ? 'active' : ''}`}
-            >
-              {index + 1}
-            </button>
-          ))}
-          
-          <button 
-            onClick={() => paginate(currentPage + 1)}
-            disabled={currentPage === totalPages}
-            className='pagination-btn'
-          >
-            Next
-          </button>
-        </div>
-      )}
-      
-      <div className='pagination-info'>
-        Showing {indexOfFirstPost + 1}-{Math.min(indexOfLastPost, posts.length)} of {posts.length} properties
       </div>
     </div>
   )
